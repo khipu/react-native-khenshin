5ccf55b9826001a8480e2961ac5d52a77c450a6d
Julio Araya <julioarayacerda@gmail.com>
2020-05-03 18:36:49 -0400

    Updating to version 1.0.3 with ios support

---
+++ ios/RNKhenshin.m
@@ -0,0 +1,68 @@
+
+#import "RNKhenshin.h"
+#import <React/RCTBridge.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import <khenshin/khenshin.h>
+
+@implementation RNKhenshin
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
+- (void) configureEngine {
+  @try {
+    __typeof(self) __weak welf = self;
+
+    [KhenshinInterface initWithBuilderBlock:^(KhenshinBuilder *builder) {
+      builder.APIUrl = @"https://khipu.com/app/enc/";
+      builder.allowCredentialsSaving = YES;
+      builder.keepCookies = NO;
+      builder.decimalSeparator = @".";
+      builder.groupingSeparator = @",";
+      builder.skipExitPage = NO;
+      builder.processHeader = nil;
+      builder.hideWebAddressInformationInForm = YES;
+      //builder.automatonTimeout = 90.0;
+    }];
+  } @catch (NSException *exception) {
+    NSLog(@"Khenshin failed to init: %@", [exception reason]);
+  }
+
+  [KhenshinInterface setAutomatonTimeout:90.0];
+}
+
+RCT_EXPORT_MODULE()
+
+// Export methods to a native module
+// https://facebook.github.io/react-native/docs/native-modules-ios.html
+RCT_EXPORT_METHOD(startPaymentById
+  : (NSString *)paymentId
+  : (RCTResponseSenderBlock) callback
+) {
+  [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+
+    if (![KhenshinInterface validateInitialization]) {
+
+            [self configureEngine];
+        }
+
+    [KhipuKhenshinInterface startEngineWithPaymentExternalId:paymentId
+          userIdentifier:@""
+       isExternalPayment:NO
+                animated:YES
+                 success:^(NSURL *returnURL) {
+                   DebugLog(@"¡Hemos vuelto!");
+                   callback(@[ @"CONCILIATING", @"{\"success\": true}" ]);
+                 }
+                 failure:^(NSURL *returnURL) {
+                   DebugLog(@"¡Hemos vuelto con error!");
+                   callback(@[ @"TASK_DUMPED", @"{\"success\": false}" ]);
+                 }];
+  }];
+}
+
+
+@end
